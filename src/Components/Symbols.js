0f45b5b0329386518cf1f39962d076b832e9efad
Raro Macias <ggmacias@gmail.com>
2022-11-07 19:36:05 -0500

    added symbols

--- src/Components/Symbols.js
+++ src/Components/Symbols.js
@@ -1,6 +1,22 @@
+import { useSelector } from "react-redux";
+import { selectDisplay } from "../redux/slices/displayCountrySlice";
+
 const Symbols = () => {
+    const currentDisplay = useSelector(selectDisplay);
     return (
-        <div>Symbols</div>
+        <div className="symbols">
+            <div className="stack">
+                <h2>Flag of {currentDisplay.name.common}:</h2>
+                {currentDisplay.flags ? (
+                    <img src={currentDisplay.flags.png} alt="Country Flag" />
+                ) : ('No Data Found')}
+            </div>
+            <div className="stack">
+                <h2> Coat of Arms of {currentDisplay.name.common}</h2>
+                {currentDisplay.coatOfArms ? (<img src={currentDisplay.coatOfArms.png} alt='Coat of Arms'/>
+                ): ('No Data Found')}
+            </div>
+        </div>
     )
 }
 export default Symbols;
